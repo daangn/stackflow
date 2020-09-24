0e46f34855a17309ac32427fd8f19f138f310cc8
Tony <jeehyukwon@gmail.com>
2020-09-24 18:37:53 +0900

    chore(example): add qrcode plugin

--- example/config-overrides.js
+++ example/config-overrides.js
@@ -1,3 +1,15 @@
-const { override,addBabelPlugin } = require('customize-cra')
+const address = require('address')
+const { override, addBabelPlugin, addWebpackPlugin } = require('customize-cra')
+const WebpackQRCodePlugin = require('@daangn/webpack-qrcode-plugin')
 
-module.exports = override(addBabelPlugin('emotion'))
+const IP = address.ip()
+const PORT = process.env.PORT || 3000
+
+module.exports = override(
+  addBabelPlugin('emotion'),
+  addWebpackPlugin(
+    new WebpackQRCodePlugin({
+      url: `karrot.alpha://minikarrot/router?remote=http://${IP}:${PORT}&scrollable=false`,
+    })
+  )
+)
