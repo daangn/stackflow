438e99e9e7c35c825f5681c0ff8bef243f4b680c
Tony <jeehyukwon@gmail.com>
2020-09-30 21:03:55 +0900

    refactor: remove unused module

--- src/utils/getDivElementsByClassName.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line
-export function getDivElementsByClassName(className: string): HTMLCollectionOf<HTMLDivElement> {
-  return document.getElementsByClassName(className) as any
-}
