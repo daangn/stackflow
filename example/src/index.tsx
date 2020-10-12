a836b59a12b3a65d430e7fb07dbebd3404b1482b
Tony <jeehyukwon@gmail.com>
2020-10-12 19:16:28 +0900

    fix: example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,9 +1,9 @@
+import './index.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
