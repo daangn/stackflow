338ac1cf87d6e3de946525e4ada7ca6a87a5b5be
juyeong1260 <juyeong1260@gmail.com>
2021-01-21 19:05:13 +0900

    fix: parse 하는 로직 최소화

--- src/navigator/useQueryParams.ts
+++ src/navigator/useQueryParams.ts
@@ -1,22 +1,27 @@
 import qs from 'querystring'
-import { useEffect, useState } from 'react'
+import { useMemo } from 'react'
 import { match, useLocation } from 'react-router-dom'
 
 import { useScreenInstanceInfo } from './contexts'
+
+const getParsedQuerystring = <T extends {} = {}>(screenPath: string) => {
+  let prevSearch = ''
+  let prevResult: T = {} as any
+
+  return (currentPath: string, search: string): T => {
+    if (currentPath === screenPath && prevSearch !== search) {
+      prevResult = qs.parse(search.split('?')[1]) as T
+      prevSearch = search
+      return prevResult
+    }
+    return prevResult
+  }
+}
 
 export function useQueryParams<T extends {} = {}>(): match<T>['params'] {
   const location = useLocation()
   const info = useScreenInstanceInfo()
 
-  const search: T = qs.parse(location.search.split('?')[1]) as any
-
-  const [state, setState] = useState<T>(search as T)
-
-  useEffect(() => {
-    if (location.pathname === info.as) {
-      setState(search)
-    }
-  }, [location.pathname, location.search])
-
-  return state
+  const _parseQuery = useMemo(() => getParsedQuerystring<T>(info.as), [info.as])
+  return useMemo(() => _parseQuery(location.pathname, location.search), [location.pathname, location.search])
 }
