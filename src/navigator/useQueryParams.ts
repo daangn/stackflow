24ce165ecff19cb22e8eff0e976cd4b5686d099b
Tony <jeehyukwon@gmail.com>
2021-01-21 22:54:48 +0900

    refactor: useQueryParams

--- src/navigator/useQueryParams.ts
+++ src/navigator/useQueryParams.ts
@@ -4,15 +4,14 @@ import { match, useLocation } from 'react-router-dom'
 
 import { useScreenInstanceInfo } from './contexts'
 
-const getParsedQuerystring = <T extends {} = {}>(screenPath: string) => {
+const getParsedQuerystring = <T extends {} = {}>({ screenPath }: { screenPath: string }) => {
   let prevSearch = ''
   let prevResult: T = {} as any
 
-  return (currentPath: string, search: string): T => {
+  return ({ currentPath, search }: { currentPath: string; search: string }): T => {
     if (currentPath === screenPath && prevSearch !== search) {
       prevResult = qs.parse(search.split('?')[1]) as T
       prevSearch = search
-      return prevResult
     }
     return prevResult
   }
@@ -22,6 +21,12 @@ export function useQueryParams<T extends {} = {}>(): match<T>['params'] {
   const location = useLocation()
   const info = useScreenInstanceInfo()
 
-  const _parseQuery = useMemo(() => getParsedQuerystring<T>(info.as), [info.as])
-  return useMemo(() => _parseQuery(location.pathname, location.search), [location.pathname, location.search])
+  const _parseQuery = useMemo(
+    () => getParsedQuerystring<T>({ screenPath: info.as }),
+    [info.as]
+  )
+  return useMemo(() => _parseQuery({ currentPath: location.pathname, search: location.search }), [
+    location.pathname,
+    location.search,
+  ])
 }
