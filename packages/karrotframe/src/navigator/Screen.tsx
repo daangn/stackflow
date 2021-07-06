5eaefaf13575cdc737d6ea72dacc1d6b091a6c3f
Tony <jeehyukwon@gmail.com>
2021-07-06 16:34:24 +0900

    fix(karrotframe): rollback breaking changes

--- packages/karrotframe/src/navigator/Screen.tsx
+++ packages/karrotframe/src/navigator/Screen.tsx
@@ -17,7 +17,7 @@ interface Props {
   /**
    * 해당 스크린에 표시할 컴포넌트
    */
-  component: React.ComponentType<ScreenComponentProps>
+  component?: React.ComponentType<ScreenComponentProps>
 }
 const Screen: React.FC<Props> = (props) => {
   const ComponentProps = props.component
@@ -25,6 +25,11 @@ const Screen: React.FC<Props> = (props) => {
   const id = useMemo(() => generateScreenId(), [])
 
   useEffect(() => {
+    if (!props.children && !ComponentProps) {
+      console.warn('component props, children 중 하나는 반드시 필요합니다')
+      return
+    }
+
     const Component: IScreen['Component'] = React.memo(
       ({ screenInstanceId, isTop, isRoot, as }) => {
         /**
@@ -53,7 +58,11 @@ const Screen: React.FC<Props> = (props) => {
                 setNavbar,
               }}
             >
-              <ComponentProps isTop={isTop} isRoot={isRoot} />
+              {ComponentProps ? (
+                <ComponentProps isTop={isTop} isRoot={isRoot} />
+              ) : (
+                props.children
+              )}
             </ScreenInstanceOptionsProvider>
           </ScreenInstanceInfoProvider>
         )
