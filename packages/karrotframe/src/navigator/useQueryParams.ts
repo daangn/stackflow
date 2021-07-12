e9908e0892d65fc9cf04fc90ca0225110f0e0624
Tony <jeehyukwon@gmail.com>
2021-07-13 02:24:43 +0900

    feat(karrotframe): useQueryParams is only for navigator

--- packages/karrotframe/src/navigator/useQueryParams.ts
+++ packages/karrotframe/src/navigator/useQueryParams.ts
@@ -3,42 +3,17 @@ import { useLocation } from 'react-router-dom'
 
 import { useScreenInstanceInfo } from './contexts'
 
-const getParsedQuerystring = <T extends { [key in keyof T]: string } = {}>({
-  screenPath,
-}: {
-  screenPath: string
-}) => {
-  let prevSearch = ''
-  let prevResult = {} as T
-
-  return ({
-    currentPath,
-    search,
-  }: {
-    currentPath: string
-    search: string
-  }): T => {
-    if (currentPath === screenPath && prevSearch !== search) {
-      prevResult = Object.fromEntries(
-        new URLSearchParams(search).entries()
-      ) as T
-      prevSearch = search
-    }
-    return prevResult
-  }
-}
-
 export function useQueryParams<
   T extends { [key in keyof T]: string } = {}
 >(): Partial<T> {
   const location = useLocation()
-  const info = useScreenInstanceInfo()
+  const { as, path } = useScreenInstanceInfo()
 
-  return useMemo(() => {
-    const parseQuery = getParsedQuerystring<T>({ screenPath: info.as })
-    return parseQuery({
-      currentPath: location.pathname,
-      search: location.search,
-    })
-  }, [info.as, location.pathname, location.search])
+  return useMemo(
+    () =>
+      (Object.fromEntries(
+        new URLSearchParams(location.search).entries()
+      ) as Partial<T>) ?? {},
+    [as, path]
+  )
 }
