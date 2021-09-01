d8b2e22f53483ff8c12c716dc634d65384227bda
Tony <jeehyukwon@gmail.com>
2021-09-01 18:26:04 +0900

    feat(pulltorefresh): init

---
+++ packages/pulltorefresh/README.md
@@ -0,0 +1,37 @@
+<img src="./cover.svg" style="width: 100%;" />
+
+<div align="center">
+
+![](https://img.shields.io/npm/v/@karrotframe/pulltorefresh)
+![](https://img.shields.io/npm/l/@karrotframe/pulltorefresh)
+![](https://img.shields.io/npm/dt/@karrotframe/pulltorefresh)
+
+</div>
+
+**Pull to Refresh UI for React**
+
+---
+
+- [Setup](#setup)
+- [Components](#components)
+- [Hooks](#hooks)
+
+---
+
+## Setup
+
+```bash
+$ yarn add @karrotframe/pulltorefresh
+```
+
+Should import the CSS of your app
+
+```typescript
+import '@karrotframe/pulltorefresh/index.css'
+
+import { ... } from '@karrotframe/pulltorefresh'
+```
+
+## Components
+
+## Hooks
---
+++ packages/pulltorefresh/webpack/build.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.resolve('./src/index.ts'),
+  },
+  output: {
+    path: path.resolve('./'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            cacheDirectory: true,
+            babelrc: true,
+          },
+        },
+      },
+      {
+        test: /\.vanilla\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              url: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.mjs', '.ts', '.tsx'],
+  },
+  externals: [/\@vanilla-extract\/dynamic/, /react/, /react-dom/],
+  devtool: 'source-map',
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new VanillaExtractPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+}
