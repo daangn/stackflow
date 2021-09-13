a0fc953d8e47e72006fc76f429130a0015f48728
Tony <jeehyukwon@gmail.com>
2021-09-13 18:03:53 +0900

    fix(example-spec): remove useLocation

--- examples/spec/src/components/_pages/PageUseQueryParams.tsx
+++ examples/spec/src/components/_pages/PageUseQueryParams.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import styled from '@emotion/styled'
 import { ScreenHelmet, useQueryParams } from '@karrotframe/navigator'
