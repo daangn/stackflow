e299f49ee9a66ed27922df633dfee3792f00b01e
irrationnelle <drakkarverenis@gmail.com>
2022-03-11 06:03:19 +0900

    fix(navigator): Add visible option for ScreenHelmet and preventSwipeBack option

--- packages/navigator/src/components/Stack.ContextScreenHelmet.tsx
+++ packages/navigator/src/components/Stack.ContextScreenHelmet.tsx
@@ -20,6 +20,7 @@ export function makeScreenHelmetDefaultProps(): IScreenHelmetProps {
     disableScrollToTop: false,
     noBorder: false,
     onTopClick: undefined,
+    preventSwipeBack: false,
   }
 }
 
