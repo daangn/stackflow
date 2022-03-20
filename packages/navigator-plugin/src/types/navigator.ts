5711ddec4c9ae7b06de632a684eedc4379dc158a
irrationnelle <drakkarverenis@gmail.com>
2022-03-20 22:24:19 +0900

    feat: Support hook when navbar is mounted and unmounted

--- packages/navigator-plugin/src/types/navigator.ts
+++ packages/navigator-plugin/src/types/navigator.ts
@@ -14,6 +14,75 @@ interface IScreenInstance {
   as: string
 }
 
+export interface IScreenHelmetProps {
+  /**
+   * title
+   */
+  title?: React.ReactNode
+
+  /**
+   * Append elements in left side
+   * (It'll be displayed in right side of back button)
+   */
+  appendLeft?: React.ReactNode
+
+  /**
+   * Append elements in right side
+   * (It'll be displayed in left side of close button when `closeButtonLocation` is `right`)
+   */
+  appendRight?: React.ReactNode
+
+  /**
+   * Location of close button (default: `left`)
+   */
+  closeButtonLocation?: 'left' | 'right'
+
+  /**
+   * Replace back button
+   */
+  customBackButton?: React.ReactNode
+
+  /**
+   * Replace close button
+   */
+  customCloseButton?: React.ReactNode
+
+  /**
+   * Remove border
+   */
+  noBorder?: boolean
+
+  /**
+   * Disable scroll to top feature (iOS Only)
+   */
+  disableScrollToTop?: boolean
+
+  /**
+   * When top part clicked (iOS Only)
+   */
+  onTopClick?: () => void
+
+  /**
+   * Set visibility for NavBar (default: `true`)
+   */
+  visible?: boolean
+
+  /**
+   * block event when users try to swipe back
+   */
+  preventSwipeBack?: boolean
+
+  /**
+   * hide back button from navbar in ScreenHelmet
+   */
+  noBackButton?: boolean
+
+  /**
+   * hide close button from navbar in ScreenHelmet
+   */
+  noCloseButton?: boolean
+}
+
 interface Options {
   pop?: (from: string) => void
   push?: (to: string) => void
@@ -108,6 +177,14 @@ export interface OnAddScreenInstancePromise extends HookParams {
   }
 }
 
+export interface OnMountNavbar extends HookParams {
+  screenHelmetProps: IScreenHelmetProps
+}
+
+export interface OnUnmountNavbar extends HookParams {
+  screenHelmetProps: IScreenHelmetProps
+}
+
 export interface PluginType {
   lifeCycleHooks: {
     beforePush?: (
@@ -170,6 +247,14 @@ export interface PluginType {
       context: OnAddScreenInstancePromise,
       next: (newCtx?: any) => Promise<OnAddScreenInstancePromise | void>
     ) => Promise<OnAddScreenInstancePromise | void>
+    onMountNavbar?: (
+      context: OnMountNavbar,
+      next: (newCtx?: any) => Promise<OnMountNavbar | void>
+    ) => Promise<OnAddScreenInstancePromise | void>
+    onUnmountNavbar?: (
+      context: OnUnmountNavbar,
+      next: (newCtx?: any) => Promise<OnUnmountNavbar | void>
+    ) => Promise<OnUnmountNavbar | void>
   }
 }
 export type NavigatorPluginType = {
