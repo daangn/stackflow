7ffcad6b28a12e6847402ad84c54fca5f83ec7f8
irrationnelle <drakkarverenis@gmail.com>
2022-04-28 14:25:57 +0900

    Revert "Chore/guide version update (#164)"
    This reverts commit 160f6f8bf16700898304454fe6f9138b18f97819.

--- packages/navigator/src/components/Card.tsx
+++ packages/navigator/src/components/Card.tsx
@@ -7,6 +7,9 @@ import { makeTranslation } from './Card.translation'
 import Navbar from './Navbar'
 import { useScreenHelmet } from './Stack.ContextScreenHelmet'
 import { useAnimationContext } from '../globalState/Animation'
+import { useMounted } from '../hooks'
+import { assignInlineVars } from '@vanilla-extract/dynamic'
+import { vars } from '../Navigator.css'
 
 interface ICardProps {
   theme: INavigatorTheme
@@ -24,6 +27,7 @@ interface ICardProps {
   onClose: () => void
 }
 const Card: React.FC<ICardProps> = (props) => {
+  const mounted = useMounted({ afterTick: true })
   const { shouldAnimate } = useAnimationContext()
 
   const { pop } = useNavigator()
@@ -169,20 +173,24 @@ const Card: React.FC<ICardProps> = (props) => {
               cupertino && isNavbarVisible ? true : undefined,
             cupertinoAndIsPresent:
               cupertino && props.isPresent ? true : undefined,
+            isTopAndIsNavbarNotVisible:
+              props.isTop && !isNavbarVisible ? true : undefined,
+          })}
+          style={assignInlineVars({
+            [vars.navbar.animationDuration]: mounted ? '0.3s' : '0',
           })}
         >
-          {isNavbarVisible && (
-            <Navbar
-              screenInstanceId={props.screenInstanceId}
-              theme={props.theme}
-              isRoot={props.isRoot}
-              isPresent={props.isPresent}
-              backButtonAriaLabel={props.backButtonAriaLabel}
-              closeButtonAriaLabel={props.closeButtonAriaLabel}
-              onClose={props.onClose}
-              onTopClick={onTopClick}
-            />
-          )}
+          <Navbar
+            isNavbarVisible={isNavbarVisible}
+            screenInstanceId={props.screenInstanceId}
+            theme={props.theme}
+            isRoot={props.isRoot}
+            isPresent={props.isPresent}
+            backButtonAriaLabel={props.backButtonAriaLabel}
+            closeButtonAriaLabel={props.closeButtonAriaLabel}
+            onClose={props.onClose}
+            onTopClick={onTopClick}
+          />
           <div
             className={css.frameOffset({
               noAnimate: !shouldAnimate ? true : undefined,
--- packages/navigator/src/helpers/nextTick.ts
+++ packages/navigator/src/helpers/nextTick.ts
@@ -1,3 +1,3 @@
 export function nextTick(callback: () => void) {
-  return Promise.resolve().then(callback)
+  setTimeout(callback, 0)
 }
