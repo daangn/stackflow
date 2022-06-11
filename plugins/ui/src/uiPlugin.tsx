7854360226c8c13fa240428a17be9319018ac841
Tony <jeehyukwon@gmail.com>
2022-06-11 15:41:16 +0900

    feat(plugin-ui): add exclude option

--- plugins/ui/src/uiPlugin.tsx
+++ plugins/ui/src/uiPlugin.tsx
@@ -9,13 +9,20 @@ const fallbackTheme = /iphone|ipad|ipod/i.test(
   ? "cupertino"
   : "android";
 
-interface UiPluginOptions {
+interface UiPluginOptions<T extends { [key: string]: any }> {
   theme?: "android" | "cupertino";
+  exclude?: Array<Extract<keyof T, string>>;
 }
-export function uiPlugin(options?: UiPluginOptions): StackflowReactPlugin {
+export function uiPlugin<T extends { [key: string]: any }>(
+  options?: UiPluginOptions<T>,
+): StackflowReactPlugin {
   return () => ({
     key: "uiPlugin",
     wrapActivity({ activity }) {
+      if (options?.exclude?.includes(activity.name as any)) {
+        return <>{activity.render()}</>;
+      }
+
       return (
         <AppScreen theme={options?.theme ?? fallbackTheme} appBar={{}}>
           {activity.render()}
