abb1b66f9d067039f59ceeaa315543cd3194e367
Tony <jeehyukwon@gmail.com>
2022-06-14 17:32:46 +0900

    docs: shorten description

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -31,7 +31,7 @@ export default {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
-        content="Mobile-first stack navigator framework with Composable Plugin System (supports iOS/Android WebView, React Native and also Desktop environment)"
+        content="Mobile-first stack navigator framework with Composable Plugin System"
       />
       <meta name="og:title" content="Stackflow" />
       <link rel="shortcut icon" href="/favicon.png" />
