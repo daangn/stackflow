9b5c3e2ca0e4dece4c7e0972e08ca23dec4adad0
Tony <jeehyukwon@gmail.com>
2022-06-18 17:28:13 +0900

    fix(docs): demo overflow in safari

--- docs/components/Demo.tsx
+++ docs/components/Demo.tsx
@@ -28,6 +28,8 @@ const Demo: React.FC = () => {
           position: "relative",
           borderRadius: ".5rem",
           overflow: "hidden",
+          transform: "translate3d(0, 0, 0)",
+          maskImage: "-webkit-radial-gradient(white, black)",
           boxShadow: "0 .25rem 1rem 0 rgba(0, 0, 0, .1)",
         }}
       >
