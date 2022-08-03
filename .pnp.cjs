6ad01aaa5c6c9441e1ac35aeff62753689584aa1
Tony <jeehyukwon@gmail.com>
2022-08-03 18:46:02 +0900

    chore: update lockfile

--- .pnp.cjs
+++ .pnp.cjs
@@ -3168,7 +3168,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@stackflow/react", "virtual:413bca98ff76262f6f1f73762ccc4b7edee04a5da42f3d6b9ed2cb2d6dbc397b2094da59b50f6e828091c88e7b5f86990feff596c43f0eb50a58fc42aae64a20#workspace:integrations/react"],\
             ["@types/react", "npm:18.0.15"],\
             ["@types/stackflow__core", null],\
-            ["@types/stackflow__plugin-history-sync", null],\
             ["@types/stackflow__react", null],\
             ["esbuild", "npm:0.14.51"],\
             ["react", "npm:18.2.0"],\
@@ -3177,11 +3176,9 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "packagePeers": [\
             "@stackflow/core",\
-            "@stackflow/plugin-history-sync",\
             "@stackflow/react",\
             "@types/react",\
             "@types/stackflow__core",\
-            "@types/stackflow__plugin-history-sync",\
             "@types/stackflow__react",\
             "react"\
           ],\
