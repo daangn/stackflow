9dea2e3c5f45587a5e64f368752c1a7af24eaa7d
Tony <jeehyukwon@gmail.com>
2022-09-20 11:33:19 +0900

    Update core/src/produceEffects.spec.ts
    Co-authored-by: kangdaehoon <kdhoon07@gmail.com>

--- core/src/produceEffects.spec.ts
+++ core/src/produceEffects.spec.ts
@@ -319,8 +319,8 @@ test("differences - 액티비티 상태가 exit-active로 변한 액티비티가
         pushedBy: {
           name: "Pushed",
         } as any,
-        isRoot: true,
-        isTop: false,
+        isRoot: false,
+        isTop: true,
       },
     },
     {
