beedccca26d93354da56bee54c7bafc8eb37401e
irrationnelle <drakkarverenis@gmail.com>
2022-11-15 11:53:43 +0900

    fix(doc): Divide scripts to support darkmode for seed scale color and Nextra theme

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -1,12 +1,14 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
-const WITH_NEXTRA_SEED_SCALE_COLOR_SCRIPT = `(() => {var e=document.documentElement;e.dataset.seed="";var pd=window.matchMedia("(prefers-color-scheme: dark)"),a=()=>{e.dataset.seedScaleColor=pd.matches?"dark":"light";e.classList.remove("light");e.classList.remove("dark");pd.matches ? e.classList.add("dark") : e.classList.add("light");};"addEventListener"in pd?pd.addEventListener("change",a):"addListener"in pd&&pd.addListener(a),a();})()`;
+const SEED_SCALE_COLOR_SCRIPT = `(() => { var e=document.documentElement;var pd=window.matchMedia("(prefers-color-scheme: dark)"),a= () => { e.dataset.seed="";e.dataset.seedScaleColor=pd.matches?"dark":"light";};"addEventListener"in pd?pd.addEventListener("change",a):"addListener"in pd&&pd.addListener(a),a();})()`;
+const NEXTRA_THEME_SCRIPT = `(() => { var e=document.documentElement;var pd=window.matchMedia("(prefers-color-scheme: dark)"),a= () => { e.classList.remove("light");e.classList.remove("dark");pd.matches ? e.classList.add("dark") : e.classList.add("light");};"addEventListener"in pd?pd.addEventListener("change",a):"addListener"in pd&&pd.addListener(a),a();})()`;
 
 export default function Document() {
   return (
     <Html data-stackflow-plugin-basic-ui-theme="cupertino">
       <Head>
-        <script dangerouslySetInnerHTML={{ __html: WITH_NEXTRA_SEED_SCALE_COLOR_SCRIPT }} />
+        <script dangerouslySetInnerHTML={{ __html: SEED_SCALE_COLOR_SCRIPT }} />
+        <script dangerouslySetInnerHTML={{ __html: NEXTRA_THEME_SCRIPT }} />
       </Head>
       <body>
         <Main />
