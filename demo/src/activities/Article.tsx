1abe39e459fb9b459a3bd805d82aa59f97677085
Tony <jeehyukwon@gmail.com>
2022-11-17 20:02:30 +0900

    fix

--- demo/src/activities/Article.tsx
+++ demo/src/activities/Article.tsx
@@ -100,7 +100,7 @@ const Article: ActivityComponentType<ArticleParams> = () => {
         <div className={css.section}>
           <div className={css.sectionTitle}>Other Items by Emila </div>
           <div className={css.recommenderGrid}>
-            {recommenderCard.map((card, i) => (
+            {recommenderCard.map((card) => (
               <ArticleCard key={card.articleId} {...card} />
             ))}
           </div>
