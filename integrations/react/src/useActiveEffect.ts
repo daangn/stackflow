da380976430c09b1d1a4c0b58092c55504bfec42
gron1gh1 <gron1gh1@gmail.com>
2022-12-12 20:59:34 +0900

    refactor(react): useActiveEffect remove prevActivity

--- integrations/react/src/useActiveEffect.ts
+++ integrations/react/src/useActiveEffect.ts
@@ -5,14 +5,8 @@ import { useActivity } from "./activity/useActivity";
 export const useActiveEffect = (effect: React.EffectCallback) => {
   const { isActive } = useActivity();
   const cleanup = useRef<ReturnType<React.EffectCallback> | null>(null);
-  const prevActive = useRef(false);
 
   useEffect(() => {
-    if (isActive === prevActive.current) {
-      return;
-    }
-
-    prevActive.current = isActive;
     if (isActive) {
       cleanup.current = effect();
     } else {
