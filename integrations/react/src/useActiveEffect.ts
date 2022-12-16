40399087cb645c4af49c793a7d257a1bb7542995
gron1gh1 <sgkang@brandazine.com>
2022-12-16 10:14:02 +0900

    refactor(react): remove cleanup ref

--- integrations/react/src/useActiveEffect.ts
+++ integrations/react/src/useActiveEffect.ts
@@ -1,16 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 import { useActivity } from "./activity/useActivity";
 
 export const useActiveEffect = (effect: React.EffectCallback) => {
   const { isActive } = useActivity();
-  const cleanup = useRef<ReturnType<React.EffectCallback> | null>(null);
 
+  // eslint-disable-next-line consistent-return
   useEffect(() => {
     if (isActive) {
-      cleanup.current = effect();
-    } else {
-      cleanup.current?.();
+      return effect();
     }
   }, [isActive]);
 };
