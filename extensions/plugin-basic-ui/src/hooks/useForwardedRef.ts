b98cb656d7dd814eb3168a4dcc52b1966f18bdff
Tony <jeehyukwon@gmail.com>
2022-12-26 15:21:40 +0900

    chore: useForwardedRef

--- extensions/plugin-basic-ui/src/hooks/useForwardedRef.ts
+++ extensions/plugin-basic-ui/src/hooks/useForwardedRef.ts
@@ -1,24 +1,11 @@
 import type React from "react";
-import { useEffect, useRef } from "react";
+import { useImperativeHandle, useRef } from "react";
 
 export function useForwardedRef<T>(
   ref: React.ForwardedRef<T>,
 ): React.RefObject<T> {
   const innerRef = useRef<T>(null);
-
-  useEffect(() => {
-    if (!ref) {
-      return;
-    }
-
-    if (typeof ref === "function") {
-      ref(innerRef.current);
-      return;
-    }
-
-    // eslint-disable-next-line no-param-reassign
-    ref.current = innerRef.current;
-  }, [ref, innerRef]);
+  useImperativeHandle<T | null, T | null>(ref, () => innerRef.current);
 
   return innerRef;
 }
