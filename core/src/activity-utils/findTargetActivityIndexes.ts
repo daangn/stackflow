5ce8ee7b301419dbeec59051fd942289d2a1cc9d
JH.Lee <contact@jins.dev>
2023-01-20 16:26:16 +0900

    fix(core): fix target activity indexes

--- core/src/activity-utils/findTargetActivityIndexes.ts
+++ core/src/activity-utils/findTargetActivityIndexes.ts
@@ -18,32 +18,40 @@ export default function findTargetActivityIndexes(
         break;
       }
 
-      const cloned = activities.slice();
-      cloned.sort((a1, a2) => a2.enteredBy.eventDate - a1.enteredBy.eventDate);
-      for (let i = 0; i < cloned.length; i += 1) {
-        targetActivities.push(i);
-        if (cloned[i].enteredBy.name === "Pushed") {
+      const sorted = activities
+        .slice()
+        .sort((a1, a2) => a2.enteredBy.eventDate - a1.enteredBy.eventDate);
+
+      for (const activity of sorted) {
+        // push original index
+        targetActivities.push(activities.indexOf(activity));
+
+        if (activity.enteredBy.name === "Pushed") {
           break;
         }
       }
       break;
     }
-    case "Popped":
+    case "Popped": {
+      const latestActivity = activities
+        .slice(1)
+        .filter((activity) => !activity.exitedBy)
+        .sort((a1, a2) => a2.enteredBy.eventDate - a1.enteredBy.eventDate)[0];
+
+      if (latestActivity) {
+        targetActivities.push(activities.indexOf(latestActivity));
+      }
+      break;
+    }
     case "StepPushed":
     case "StepReplaced":
     case "StepPopped": {
-      let targetIdx = 0;
-      let { eventDate } = activities[0].enteredBy;
+      const latestActivity = activities
+        .filter((activity) => !activity.exitedBy)
+        .sort((a1, a2) => a2.enteredBy.eventDate - a1.enteredBy.eventDate)[0];
 
-      for (let i = 1; i < activities.length; i += 1) {
-        if (activities[i].enteredBy.eventDate > eventDate) {
-          targetIdx = i;
-          eventDate = activities[i].enteredBy.eventDate;
-        }
-      }
-
-      if (event.name !== "Popped" || targetIdx !== 0) {
-        targetActivities.push(targetIdx);
+      if (latestActivity) {
+        targetActivities.push(activities.indexOf(latestActivity));
       }
       break;
     }
