725e972ad4929cf5b0f75129f6f2ceedc4672b49
junghyeonsu <jung660317@naver.com>
2023-03-10 15:59:59 +0900

    feat(docs): generate plugins docs

---
+++ scripts/generate-plugins-docs.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+const extensionsFolderPath = "./extensions";
+const pluginsFolderPath = "./docs/pages/plugins";
+
+const githubBaseUrl = "https://github.com/daangn/stackflow";
+const extensionsPathUrl = `${githubBaseUrl}/tree/main/extensions`;
+
+console.log("Generate plugins docs start");
+
+fs.readdir(extensionsFolderPath, (err, files) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // -> write to docs/pages/plugins/meta.ko.json
+  fs.writeFileSync(
+    path.resolve(pluginsFolderPath, "meta.ko.json"),
+    `{\n${files.map((file) => `  "${file}": "${file}"`).join(",\n")}\n}\n`,
+  );
+
+  files.forEach((file) => {
+    const pluginFolderPath = path.join(extensionsFolderPath, file);
+    if (fs.statSync(pluginFolderPath).isDirectory()) {
+      const readmeFilePath = path.join(pluginFolderPath, "README.md");
+      fs.readFile(readmeFilePath, "utf8", (err, data) => {
+        if (err) {
+          console.log(err);
+          process.exit(1);
+        }
+
+        // -> write to docs/pages/plugins/${file}.ko.mdx
+        fs.writeFileSync(
+          path.resolve(pluginsFolderPath, `${file}.ko.mdx`),
+          `[plugin github link](${extensionsPathUrl}/${file})\n\n${data}`,
+        );
+      });
+    }
+  });
+});
+
+console.log("Generate plugins docs success!");
