911f29882f070c94d031ad4a54599b2ba5f101b8
JH.Lee <contact@jins.dev>
2023-03-30 11:46:29 +0900

    v0.1.0

--- devtools/src/manifest.ts
+++ devtools/src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   name: "Stackflow Devtools",
   description: "Devtools for Stackflow Apps",
-  version: "0.0.0",
+  version: "0.1.0",
   manifest_version: 3,
   action: {
     default_popup: "index.html",
