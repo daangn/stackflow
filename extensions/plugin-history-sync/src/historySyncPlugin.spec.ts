d3f1f5ece9ff7acd07a4e46819ad278a2c26b8a3
irrationnelle <drakkarverenis@gmail.com>
2023-05-09 00:03:57 +0900

    test(plugin-history-sync): Add test to restore replacePopCount

--- extensions/plugin-history-sync/src/historySyncPlugin.spec.ts
+++ extensions/plugin-history-sync/src/historySyncPlugin.spec.ts
@@ -805,7 +805,7 @@ describe("historySyncPlugin", () => {
 
     // 전환이 끝나기까지 충분한 시간
     // 코어쪽 추가된 테스트 코드가 Resolve 되면 삭제 가능
-    await delay(16 * 7);
+    await delay(ENOUGH_DELAY_TIME);
 
     expect(path(history.location)).toEqual("/home/");
     expect(activeActivity(actions.getStack())?.name).toEqual("Home");
@@ -947,12 +947,159 @@ describe("historySyncPlugin", () => {
         thirdId: "4",
       },
     });
+    actions.pop();
 
     actions.replace({
       activityId: "a4",
       activityName: "FourthActivity",
       activityParams: {
         fourthId: "345",
+      },
+    });
+    await delay(ENOUGH_DELAY_TIME);
+    actions.pop();
+
+    await delay(ENOUGH_DELAY_TIME);
+
+    expect(path(history.location)).toEqual("/home/");
+    expect(activeActivity(actions.getStack())?.name).toEqual("Home");
+  })
+
+  test("historySyncPlugin - 2회 반복:push 후 stepPush 를 반복한 뒤, push 를 수행하고 stepPush 를 반복한 뒤 pop 을 수행, 그 후 replace 를 수행하고 pop을 진행해도 첫번째 stack을 가리킵니다.", async () => {
+    actions.push({
+      activityId: "a2",
+      activityName: "Article",
+      activityParams: {
+        articleId: "1",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s2",
+      stepParams: {
+        articleId: "2",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s3",
+      stepParams: {
+        articleId: "3",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s4",
+      stepParams: {
+        articleId: "4",
+      },
+    });
+
+    actions.push({
+      activityId: "a3",
+      activityName: "ThirdActivity",
+      activityParams: {
+        thirdId: "234",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s2",
+      stepParams: {
+        thirdId: "2",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s3",
+      stepParams: {
+        thirdId: "3",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s4",
+      stepParams: {
+        thirdId: "4",
+      },
+    });
+    actions.pop();
+
+    actions.replace({
+      activityId: "a4",
+      activityName: "FourthActivity",
+      activityParams: {
+        fourthId: "345",
+      },
+    });
+    await delay(ENOUGH_DELAY_TIME);
+    actions.pop();
+
+    await delay(ENOUGH_DELAY_TIME);
+    actions.push({
+      activityId: "a5",
+      activityName: "ThirdActivity",
+      activityParams: {
+        thirdId: "1",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s2",
+      stepParams: {
+        thirdId: "2",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s3",
+      stepParams: {
+        thirdId: "3",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s4",
+      stepParams: {
+        thirdId: "4",
+      },
+    });
+
+    actions.push({
+      activityId: "a6",
+      activityName: "FourthActivity",
+      activityParams: {
+        fourthId: "234",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s2",
+      stepParams: {
+        fourthId: "2",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s3",
+      stepParams: {
+        fourthId: "3",
+      },
+    });
+
+    actions.stepPush({
+      stepId: "s4",
+      stepParams: {
+        fourthId: "4",
+      },
+    });
+    actions.pop();
+
+    actions.replace({
+      activityId: "a7",
+      activityName: "Article",
+      activityParams: {
+        articleId: "345",
       },
     });
     await delay(ENOUGH_DELAY_TIME);
