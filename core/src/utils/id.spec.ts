908e4ed4265cb1cdccd1d1f560265c89006c1c78
ssi02014 <ssi02014@naver.com>
2023-08-06 02:17:41 +0900

    refac(stackflow): Test 코드 내 변수 네이밍 변경

--- core/src/utils/id.spec.ts
+++ core/src/utils/id.spec.ts
@@ -7,20 +7,20 @@ beforeAll(() => {
 const getCalculatedId = (time: number) => (time * 1000).toString(16);
 
 test("id - time() 값을 기반으로 유니크한 아이디 값을 반환한다.", () => {
-  const time1 = new Date("2023-08-06").getTime(); // 1691280000000
-  const time2 = new Date("2023-08-07").getTime(); // 1691366400000
+  const expectedTime1 = new Date("2023-08-06").getTime(); // 1691280000000
+  const expectedTime2 = new Date("2023-08-07").getTime(); // 1691366400000
 
-  jest.setSystemTime(time1);
+  jest.setSystemTime(expectedTime1);
 
-  expect(id()).toBe(getCalculatedId(time1));
-  expect(id()).toBe(getCalculatedId((time1 * 1000 + 1) / 1000));
-  expect(id()).toBe(getCalculatedId((time1 * 1000 + 2) / 1000));
-  expect(id()).toBe(getCalculatedId((time1 * 1000 + 3) / 1000));
+  expect(id()).toBe(getCalculatedId(expectedTime1));
+  expect(id()).toBe(getCalculatedId((expectedTime1 * 1000 + 1) / 1000));
+  expect(id()).toBe(getCalculatedId((expectedTime1 * 1000 + 2) / 1000));
+  expect(id()).toBe(getCalculatedId((expectedTime1 * 1000 + 3) / 1000));
 
-  jest.setSystemTime(time2);
+  jest.setSystemTime(expectedTime2);
 
-  expect(id()).toBe(getCalculatedId(time2));
-  expect(id()).toBe(getCalculatedId((time2 * 1000 + 1) / 1000));
-  expect(id()).toBe(getCalculatedId((time2 * 1000 + 2) / 1000));
-  expect(id()).toBe(getCalculatedId((time2 * 1000 + 3) / 1000));
+  expect(id()).toBe(getCalculatedId(expectedTime2));
+  expect(id()).toBe(getCalculatedId((expectedTime2 * 1000 + 1) / 1000));
+  expect(id()).toBe(getCalculatedId((expectedTime2 * 1000 + 2) / 1000));
+  expect(id()).toBe(getCalculatedId((expectedTime2 * 1000 + 3) / 1000));
 });
--- core/src/utils/time.spec.ts
+++ core/src/utils/time.spec.ts
@@ -5,20 +5,20 @@ beforeAll(() => {
 });
 
 test("time - 이전 호출과 현재 호출 사이에 시간이 동일한 경우 중복을 방지한다.", () => {
-  const time1 = new Date("2023-08-06").getTime(); // 1691280000000
-  const time2 = new Date("2023-08-07").getTime(); // 1691366400000
+  const expectedTime1 = new Date("2023-08-06").getTime(); // 1691280000000
+  const expectedTime2 = new Date("2023-08-07").getTime(); // 1691366400000
 
-  jest.setSystemTime(time1);
+  jest.setSystemTime(expectedTime1);
 
-  expect(time()).toBe(time1);
-  expect(time()).toBe((time1 * 1000 + 1) / 1000);
-  expect(time()).toBe((time1 * 1000 + 2) / 1000);
-  expect(time()).toBe((time1 * 1000 + 3) / 1000);
+  expect(time()).toBe(expectedTime1);
+  expect(time()).toBe((expectedTime1 * 1000 + 1) / 1000);
+  expect(time()).toBe((expectedTime1 * 1000 + 2) / 1000);
+  expect(time()).toBe((expectedTime1 * 1000 + 3) / 1000);
 
-  jest.setSystemTime(time2);
+  jest.setSystemTime(expectedTime2);
 
-  expect(time()).toBe(time2);
-  expect(time()).toBe((time2 * 1000 + 1) / 1000);
-  expect(time()).toBe((time2 * 1000 + 2) / 1000);
-  expect(time()).toBe((time2 * 1000 + 3) / 1000);
+  expect(time()).toBe(expectedTime2);
+  expect(time()).toBe((expectedTime2 * 1000 + 1) / 1000);
+  expect(time()).toBe((expectedTime2 * 1000 + 2) / 1000);
+  expect(time()).toBe((expectedTime2 * 1000 + 3) / 1000);
 });
