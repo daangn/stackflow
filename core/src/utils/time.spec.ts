13b2afaf07fd2b4a2ed896cec40c6f61c5b207f0
ssi02014 <ssi02014@naver.com>
2023-08-06 02:14:56 +0900

    fix(stackflow): Fix core/utils/time Test Code

--- core/src/utils/time.spec.ts
+++ core/src/utils/time.spec.ts
@@ -4,7 +4,7 @@ beforeAll(() => {
   jest.useFakeTimers();
 });
 
-test("time - 이전 호출과 현재 호출 사이에 시간이 동일한 경우 시간의 중복을 방지한다.", () => {
+test("time - 이전 호출과 현재 호출 사이에 시간이 동일한 경우 중복을 방지한다.", () => {
   const time1 = new Date("2023-08-06").getTime(); // 1691280000000
   const time2 = new Date("2023-08-07").getTime(); // 1691366400000
 
