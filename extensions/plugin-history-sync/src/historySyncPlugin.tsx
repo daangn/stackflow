177a60aa22f4305bb158b2ed838d8a8d84359747
JH.Lee <contact@jins.dev>
2023-09-18 01:16:40 +0900

    temp

--- extensions/plugin-history-sync/src/historySyncPlugin.tsx
+++ extensions/plugin-history-sync/src/historySyncPlugin.tsx
@@ -19,22 +19,49 @@ import { RoutesProvider } from "./RoutesContext";
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 
-type HistorySyncPluginOptions<K extends string> = {
+type RemoveTail<
+  S extends string,
+  Tail extends string,
+> = S extends `${infer P}${Tail}` ? P : S;
+type GetRouteParameter<S extends string> = RemoveTail<
+  RemoveTail<RemoveTail<S, `/${string}`>, `-${string}`>,
+  `.${string}`
+>;
+
+export type RouteParameters<Route extends string> = string extends Route
+  ? Record<string, string>
+  : Route extends `${string}(${string}`
+  ? Record<string, string>
+  : Route extends `${string}:${infer Rest}`
+  ? (GetRouteParameter<Rest> extends never
+      ? Record<string, string>
+      : GetRouteParameter<Rest> extends `${infer ParamName}?`
+      ? { [P in ParamName]?: string }
+      : { [P in GetRouteParameter<Rest>]: string }) &
+      (Rest extends `${GetRouteParameter<Rest>}${infer Next}`
+        ? RouteParameters<Next>
+        : unknown)
+  : {};
+
+type Route<K, T extends string = string> = {
+  path: T;
+  decode: (params: RouteParameters<T>) => K;
+};
+
+type HistorySyncPluginOptions<T, K extends keyof T> = {
   routes: {
-    [key in K]: string | string[];
+    [key in K]: string | string[] | Route<T[key]> | Route<T[key]>[];
   };
   fallbackActivity: (args: { initialContext: any }) => K;
   useHash?: boolean;
   history?: History;
   urlPatternOptions?: UrlPatternOptions;
 };
+
 export function historySyncPlugin<
   T extends { [activityName: string]: unknown },
->(
-  options: HistorySyncPluginOptions<Extract<keyof T, string>>,
-): StackflowReactPlugin {
-  type K = Extract<keyof T, string>;
-
+  K extends Extract<keyof T, string>,
+>(options: HistorySyncPluginOptions<T, K>): StackflowReactPlugin {
   const history =
     options.history ??
     (typeof window === "undefined"
