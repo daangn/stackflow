44702a9897053fd675110771831c80894a354e4b
Tony Won <tony@daangn.com>
2024-07-19 02:35:25 +0900

    feat(react): `@stackflow/react/future`

--- extensions/link/src/Link.tsx
+++ extensions/link/src/Link.tsx
@@ -1,8 +1,8 @@
 import type { UrlPatternOptions } from "@stackflow/plugin-history-sync";
 import { makeTemplate, useRoutes } from "@stackflow/plugin-history-sync";
 import { usePreloader } from "@stackflow/plugin-preload";
-import type { ActivityComponentType } from "@stackflow/react";
 import { useActions } from "@stackflow/react";
+import type { ActivityComponentType } from "@stackflow/react";
 import { forwardRef, useEffect, useMemo, useReducer, useRef } from "react";
 
 import { mergeRefs } from "./mergeRefs";
--- integrations/react/esbuild.config.js
+++ integrations/react/esbuild.config.js
@@ -26,4 +26,50 @@ Promise.all([
   }).then((ctx) =>
     watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
   ),
+  context({
+    ...config({
+      entryPoints: ["./src/stable/index.ts"],
+      outdir: "./dist/stable",
+    }),
+    format: "cjs",
+    external,
+  }).then((ctx) =>
+    watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
+  ),
+  context({
+    ...config({
+      entryPoints: ["./src/stable/index.ts"],
+      outdir: "./dist/stable",
+    }),
+    format: "esm",
+    outExtension: {
+      ".js": ".mjs",
+    },
+    external,
+  }).then((ctx) =>
+    watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
+  ),
+  context({
+    ...config({
+      entryPoints: ["./src/future/index.ts"],
+      outdir: "./dist/future",
+    }),
+    format: "cjs",
+    external,
+  }).then((ctx) =>
+    watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
+  ),
+  context({
+    ...config({
+      entryPoints: ["./src/future/index.ts"],
+      outdir: "./dist/future",
+    }),
+    format: "esm",
+    outExtension: {
+      ".js": ".mjs",
+    },
+    external,
+  }).then((ctx) =>
+    watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
+  ),
 ]).catch(() => process.exit(1));
