aa3814114dd4cb0fdcb26c611606c16072e609b9
Tony Won <tony@daangn.com>
2024-07-30 18:50:13 +0900

    WIP: change stackflow ESM Only

--- integrations/react/esbuild.config.js
+++ integrations/react/esbuild.config.js
@@ -9,23 +9,9 @@ Promise.all([
       entryPoints: ["./src/**/*"],
     }),
     bundle: false,
-    external: undefined,
-    format: "cjs",
-    sourcemap: false,
-  }).then((ctx) =>
-    watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
-  ),
-  context({
-    ...config({
-      entryPoints: ["./src/**/*"],
-    }),
-    bundle: false,
-    external: undefined,
     format: "esm",
-    outExtension: {
-      ".js": ".mjs",
-    },
     sourcemap: false,
+    external: undefined,
   }).then((ctx) =>
     watch ? ctx.watch() : ctx.rebuild().then(() => ctx.dispose()),
   ),
