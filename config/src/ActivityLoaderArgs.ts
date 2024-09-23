e8dbb242b04d01afe932140ec605b0e8489621d7
iamchanii <contact@imch.dev>
2024-09-23 11:39:48 +0900

    Revert params optional

--- config/src/ActivityLoaderArgs.ts
+++ config/src/ActivityLoaderArgs.ts
@@ -5,5 +5,5 @@ import type { RegisterActivityLoaderArgs } from "./RegisterActivityLoaderArgs"
 export interface ActivityLoaderArgs<
   ActivityName extends Extract<keyof RegisteredActivityParamTypes, string>,
 > extends RegisterActivityLoaderArgs {
-  params?: InferActivityParams<ActivityName>;
+  params: InferActivityParams<ActivityName>;
 };
