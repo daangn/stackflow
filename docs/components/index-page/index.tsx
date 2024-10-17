f46d6bcd603ff50a5ddd6eeeede79d770f39f8be
dylan.lee <ho991217@kakao.com>
2024-10-18 02:46:59 +0900

    Improve home page

--- docs/components/index-page/index.tsx
+++ docs/components/index-page/index.tsx
@@ -18,13 +18,13 @@ export function IndexPage({ lang = "ko" }: { lang: "ko" | "en" }) {
 
   return (
     <main className="w-full lg:max-w-[1392px] mx-auto">
-      <section className="relative w-full lg:min-h-[1000px] grid lg:grid-cols-2 place-items-center">
-        <div className="flex flex-col items-center gap-2 my-8 lg:ml-8 lg:items-start lg:gap-0 lg:m-0">
+      <section className="relative w-full grid py-8 lg:py-24 lg:grid-cols-2 place-items-center">
+        <div className="flex flex-col items-center gap-4 my-8 lg:ml-8 lg:items-start lg:gap-1 lg:m-0">
           <h1 className="text-5xl lg:text-8xl font-extrabold">Stackflow</h1>
 
           <p className="text-sm lg:text-xl text-center">
             {getLocaleText(
-              "JavaScript와 TypeScript를 위한 가장 간편한 스택 네비게이션 라이브러리.",
+              "JavaScript와 TypeScript를 위한 가장 간편한 스택 네비게이션 프레임워크.",
               "The Simplest Stack Navigation for JavaScript and TypeScript.",
             )}
           </p>
@@ -48,7 +48,7 @@ export function IndexPage({ lang = "ko" }: { lang: "ko" | "en" }) {
         <div
           ref={ref}
           className={cn(
-            "w-full max-w-[350px] h-[650px] lg:max-w-[450px] lg:h-[900px] relative flex",
+            "w-full max-w-[350px] h-[650px] lg:max-w-[450px] lg:h-[800px] relative flex",
           )}
           style={style}
         >
@@ -65,49 +65,72 @@ export function IndexPage({ lang = "ko" }: { lang: "ko" | "en" }) {
         />
       </section>
 
-      <section className="w-full grid grid-cols-3 gap-8 px-6 pb-32">
-        <Tile
-          title="Headless Architecture"
-          content={getLocaleText(
-            "사전 구성된 UI 없이도 스택과 전이 상태를 사용하여 완전한 커스터마이징이 가능합니다.",
-            "Use stack and transition states without pre-built UI, allowing full customization.",
-          )}
-        />
-        <Tile
-          title="Mobile-first Architecture"
-          content={getLocaleText(
-            "모바일 환경을 위해 최적화된 설계.",
-            "Optimized for mobile environments.",
-          )}
-        />
-        <Tile
-          title="Composablity"
-          content={getLocaleText(
-            "플러그인 인터페이스를 통해 어느 라이프사이클에서든지 커스텀 기능을 주입하세요.",
-            "Inject custom functionality via plugin interfaces at any lifecycle stage.",
-          )}
-        />
-        <Tile
-          title="Framework Agnostic"
-          content={getLocaleText(
-            "분리된 코어 로직 덕분에 다양한 프론트엔드 프레임워크와 매끄럽게 통합됩니다.",
-            "Seamlessly integrate with diverse frontend frameworks due to its decoupled core logic.",
-          )}
-        />
-        <Tile
-          title="Cross-Platform Rendering"
-          content={getLocaleText(
-            "단일 코드베이스로 모바일 웹 뷰와 데스크톱 앱을 빌드하기 위한 커스텀 렌더 로직 주입이 가능합니다.",
-            "Inject custom render logic to build both mobile web views and desktop apps from a single codebase.",
-          )}
-        />
-        <Tile
-          title="Server-side Rendering & TypeScript Support"
-          content={getLocaleText(
-            "서버사이드 렌더링과 타입스크립트를 완벽하게 지원합니다.",
-            "Fully supports server-side rendering and TypeScript.",
-          )}
-        />
+      <section className="w-full flex flex-col items-center px-6 pb-8 lg:pb-32 gap-16">
+        <div className="flex text-xl lg:text-4xl gap-1">
+          "
+          <h1 className="text-xl text-center lg:text-3xl font-bold">
+            Mobile-first Stack Navigator Framework with Composable Plugin System
+          </h1>
+          "
+        </div>
+        <div className="grid lg:grid-cols-3 gap-8">
+          <Tile
+            title="Headless Architecture"
+            content={getLocaleText(
+              "사전 구성된 UI 없이도 스택과 전이 상태를 사용하여 완전한 커스터마이징이 가능합니다.",
+              "Use stack and transition states without pre-built UI, allowing full customization.",
+            )}
+          />
+          <Tile
+            title="Mobile-first Architecture"
+            content={getLocaleText(
+              "모바일 환경을 위해 최적화된 설계.",
+              "Optimized for mobile environments.",
+            )}
+          />
+          <Tile
+            title="Composablity"
+            content={getLocaleText(
+              "플러그인 인터페이스를 통해 어느 라이프사이클에서든지 커스텀 기능을 주입하세요.",
+              "Inject custom functionality via plugin interfaces at any lifecycle stage.",
+            )}
+          />
+          <Tile
+            title="Framework Agnostic"
+            content={getLocaleText(
+              "분리된 코어 로직 덕분에 다양한 프론트엔드 프레임워크와 매끄럽게 통합됩니다.",
+              "Seamlessly integrate with diverse frontend frameworks due to its decoupled core logic.",
+            )}
+          />
+          <Tile
+            title="Cross-Platform Rendering"
+            content={getLocaleText(
+              "단일 코드베이스로 모바일 웹 뷰와 데스크톱 앱을 빌드하기 위한 커스텀 렌더 로직 주입이 가능합니다.",
+              "Inject custom render logic to build both mobile web views and desktop apps from a single codebase.",
+            )}
+          />
+          <Tile
+            title="Server-side Rendering & TypeScript Support"
+            content={getLocaleText(
+              "서버사이드 렌더링과 타입스크립트를 완벽하게 지원합니다.",
+              "Fully supports server-side rendering and TypeScript.",
+            )}
+          />
+        </div>
+      </section>
+
+      <section className="w-full flex flex-col items-center px-6 py-32 gap-16">
+        <h1 className="text-2xl lg:text-4xl font-bold text-center">
+          Driven by the Community
+        </h1>
+        <Link href="https://github.com/daangn/stackflow/graphs/contributors">
+          <Image
+            src="https://contrib.rocks/image?repo=daangn/stackflow"
+            alt=""
+            width={800}
+            height={800}
+          />
+        </Link>
       </section>
     </main>
   );
