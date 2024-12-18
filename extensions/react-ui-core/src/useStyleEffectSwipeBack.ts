e328848cf6541d72944d5ffa9fb67a136eea012e
Tony Won <tony@daangn.com>
2024-12-18 20:56:27 +0900

    fix: `_swiped` to `swiped`

--- extensions/react-ui-core/src/useStyleEffectSwipeBack.ts
+++ extensions/react-ui-core/src/useStyleEffectSwipeBack.ts
@@ -181,7 +181,7 @@ export function useStyleEffectSwipeBack({
                 );
               });
 
-              onTransitionEnd?.({ swiped: _swiped });
+              onTransitionEnd?.({ swiped });
             });
           });
         });
