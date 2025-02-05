89d91c16c51a3d5d158a1014f814f5ec95056c25
ENvironmentSet <sbshw1@gmail.com>
2025-02-05 17:59:38 +0900

    fix

--- integrations/react/src/future/useStepFlow.ts
+++ integrations/react/src/future/useStepFlow.ts
@@ -2,8 +2,6 @@ import type {
   InferActivityParams,
   RegisteredActivityName,
 } from "@stackflow/config";
-import { useContext } from "react";
-import { ActivityContext } from "../__internal__/activity";
 import { useCoreActions } from "../__internal__/core";
 import type { StepActions } from "./StepActions";
 import { makeStepActions } from "./makeStepActions";
@@ -12,7 +10,6 @@ export function useStepFlow<ActivityName extends RegisteredActivityName>(
   activityName: ActivityName,
 ): StepActions<InferActivityParams<ActivityName>> {
   const coreActions = useCoreActions();
-  const { id } = useContext(ActivityContext);
 
   return makeStepActions(() => coreActions);
 }
