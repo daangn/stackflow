f63230a692406c78f5e1f878cf7f48c2fc3c4189
ENvironmentSet <sbshw1@gmail.com>
2025-07-25 14:25:04 +0900

    feat(plugin-history-sync): Default history setup option for rich deep link experiences.

--- extensions/plugin-history-sync/src/RouteLike.ts
+++ extensions/plugin-history-sync/src/RouteLike.ts
@@ -1,11 +1,26 @@
+import type {
+  RegisteredActivityName,
+  RegisteredActivityParamTypes,
+} from "@stackflow/config";
 import type { ActivityComponentType } from "@stackflow/react";
+import type { NonEmptyArray } from "./NonEmptyArray";
 
 export type Route<ComponentType> = {
   path: string;
   decode?: (
     params: Record<string, string>,
   ) => ComponentType extends ActivityComponentType<infer U> ? U : {};
+  defaultHistory?: NonEmptyArray<HistoryEntry>;
 };
+
+export type HistoryEntry = {
+  [K in RegisteredActivityName]: {
+    activityName: K;
+    decode: (
+      params: Record<string, string>,
+    ) => NonEmptyArray<RegisteredActivityParamTypes[K]>;
+  };
+}[RegisteredActivityName];
 
 export type RouteLike<ComponentType> =
   | string
