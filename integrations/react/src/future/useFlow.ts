4c9ce281884c7ad4dac7ddc271b2998e92590c31
ENvironmentSet <sbshw1@gmail.com>
2025-07-31 15:43:37 +0900

    test

--- integrations/react/src/future/useFlow.ts
+++ integrations/react/src/future/useFlow.ts
@@ -1,12 +1,39 @@
+import type { RegisteredActivityName } from "@stackflow/config";
+import { useEffect } from "react";
 import { useCoreActions } from "../__internal__/core";
 import type { Actions } from "./Actions";
+import { useActivityComponentMap } from "./ActivityComponentMapProvider";
 import { makeActions } from "./makeActions";
+import { useDataLoader } from "./stackflow";
+import { useConfig } from "./useConfig";
 
 export type FlowOutput = {
   useFlow: () => Actions;
 };
 
-export function useFlow(): Actions {
+export interface UseFlowOptions {
+  relatedActivities?: RegisteredActivityName[];
+}
+
+export function useFlow(options?: UseFlowOptions): Actions {
   const coreActions = useCoreActions();
-  return makeActions(() => coreActions);
+  const config = useConfig();
+  const activityComponentMap = useActivityComponentMap();
+  const loadData = useDataLoader();
+  const actions = makeActions(
+    () => config,
+    () => coreActions,
+    activityComponentMap,
+    loadData,
+  );
+
+  useEffect(() => {
+    if (options?.relatedActivities) {
+      for (const activityName of options.relatedActivities) {
+        actions.prepare(activityName);
+      }
+    }
+  }, [options?.relatedActivities]);
+
+  return actions;
 }
