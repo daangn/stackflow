7770a81d736e549b11224e1fba615430b27d49b1
JH.Lee <contact@jins.dev>
2025-08-04 12:20:50 +0900

    fix: handle memo component

--- integrations/react/src/stable/lazyActivityPlugin.ts
+++ integrations/react/src/stable/lazyActivityPlugin.ts
@@ -1,10 +1,12 @@
 import type { ActivityComponentType } from "../__internal__/ActivityComponentType";
 import type { StackflowReactPlugin } from "../__internal__/StackflowReactPlugin";
 
-// https://github.com/facebook/react/blob/v19.1.1/packages/react/src/ReactLazy.js
 
+// https://github.com/facebook/react/blob/v19.1.1/packages/shared/ReactSymbols.js#L32
 const REACT_LAZY_TYPE: symbol = Symbol.for("react.lazy");
+const REACT_MEMO_TYPE: symbol = Symbol.for('react.memo');
 
+// https://github.com/facebook/react/blob/v19.1.1/packages/react/src/ReactLazy.js
 interface Wakeable {
   then(onFulfill: () => unknown, onReject: () => unknown): undefined | Wakeable;
 }
@@ -75,58 +77,69 @@ type LazyComponent = {
   _payload: Payload<unknown>;
 };
 
+type MemoComponent = {
+  $$typeof: symbol | number;
+  type: React.ElementType;
+};
+
 function isLazyComponent(component: unknown): component is LazyComponent {
-  return (
+  const isLazy = (
     typeof component === "object" &&
     component !== null &&
     "$$typeof" in component &&
-    component.$$typeof === REACT_LAZY_TYPE &&
-    "_payload" in component
+      component.$$typeof === REACT_LAZY_TYPE &&
+      "_payload" in component
+    );
+  return isLazy;
+}
+
+function isMemoComponent(component: unknown): component is MemoComponent {
+  const isMemo = (
+    typeof component === "object" &&
+    component !== null &&
+    "$$typeof" in component &&
+    component.$$typeof === REACT_MEMO_TYPE
   );
+  return isMemo;
 }
 
 export function lazyActivityPlugin(activityComponentMap: {
   [key: string]: ActivityComponentType;
 }): StackflowReactPlugin {
+  function handleLazyActivity({ actions, actionParams }: {
+    actions: { pause: () => void; resume: () => void };
+    actionParams: { activityName: string };
+  }) {
+    let Activity = activityComponentMap[actionParams.activityName];
+
+    if (isMemoComponent(Activity)) {
+      Activity = Activity.type as ActivityComponentType;
+    }
+
+    if (
+      isLazyComponent(Activity) &&
+      Activity._payload._status === Uninitialized
+    ) {
+      actions.pause();
+
+      Activity._payload._result().then(
+        () => {
+          actions.resume();
+        },
+        () => {
+          actions.resume();
+        },
+      );
+    }
+  }
+
   return () => ({
     key: "plugin-lazy-activity",
     onBeforePush({ actions, actionParams }) {
-      const Activity = activityComponentMap[actionParams.activityName];
-
-      if (
-        isLazyComponent(Activity) &&
-        Activity._payload._status === Uninitialized
-      ) {
-        actions.pause();
-
-        Activity._payload._result().then(
-          () => {
-            actions.resume();
-          },
-          () => {
-            actions.resume();
-          },
-        );
-      }
+      handleLazyActivity({ actions, actionParams });
     },
     onBeforeReplace({ actions, actionParams }) {
-      const Activity = activityComponentMap[actionParams.activityName];
-
-      if (
-        isLazyComponent(Activity) &&
-        Activity._payload._status === Uninitialized
-      ) {
-        actions.pause();
-
-        Activity._payload._result().then(
-          () => {
-            actions.resume();
-          },
-          () => {
-            actions.resume();
-          },
-        );
-      }
+      handleLazyActivity({ actions, actionParams });
     },
   });
 }
