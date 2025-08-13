2a5d13611355f63ee28e7b91fcb64c521a2b29e2
anakin_karrot <anakin@daangn.com>
2025-08-13 18:37:03 +0900

    fix type

--- integrations/react/src/__internal__/LazyActivityComponentType.ts
+++ integrations/react/src/__internal__/LazyActivityComponentType.ts
@@ -1,6 +1,7 @@
 import type { StaticActivityComponentType } from "./StaticActivityComponentType";
+import type { ComponentType } from "react";
 
 export type LazyActivityComponentType<T extends { [K in keyof T]: any } = {}> =
-  React.ComponentType<{ params: T; shouldRenderImmediately?: boolean }> & {
+  ComponentType<{ params: T; shouldRenderImmediately?: boolean }> & {
     _load?: () => Promise<{ default: StaticActivityComponentType<T> }>;
   };
