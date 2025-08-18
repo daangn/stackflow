55f23c8483b6f4fc22e15609ea081ffbbf3c3b01
anakin_karrot <anakin@daangn.com>
2025-08-18 18:35:07 +0900

    update

--- .changeset/slick-heads-tan.md
+++ .changeset/slick-heads-tan.md
@@ -4,4 +4,4 @@
 
 Add prefetch API for lazy activity component and loader data.
 - A hook `usePrepare()` which returns `prepare(activityName[, activityParams])` is added for navigation warmup.
-- A hook `useActivityPreparation(activityNames)` for preparing navigations inside a component is added.
+- A hook `useActivityPreparation(activities)` for preparing navigations inside a component is added.
--- integrations/react/src/future/useActivityPreparation.ts
+++ integrations/react/src/future/useActivityPreparation.ts
@@ -1,10 +1,12 @@
 import type { RegisteredActivityName } from "@stackflow/config";
 import { usePrepare } from "./usePrepare";
 
-export function useActivityPreparation(activities: RegisteredActivityName[]) {
+export function useActivityPreparation(
+  activities: { activityName: RegisteredActivityName }[],
+) {
   const prepare = usePrepare();
 
-  for (const activityName of activities) {
+  for (const { activityName } of activities) {
     prepare(activityName);
   }
 }
