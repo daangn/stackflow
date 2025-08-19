4955f925cb5cea1458afb2cf3d3317ffa328c20e
anakin_karrot <anakin@daangn.com>
2025-08-19 11:48:14 +0900

    remove unused file

--- integrations/react/src/future/ActivityComponentType.tsx
+++
@@ -1,8 +0,0 @@
-import type {
-  InferActivityParams,
-  RegisteredActivityName,
-} from "@stackflow/config";
-import type React from "react";
-
-export type ActivityComponentType<ActivityName extends RegisteredActivityName> =
-  React.ComponentType<{ params: InferActivityParams<ActivityName> }>;
