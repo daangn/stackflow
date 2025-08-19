18ca6143aa95b872d6671892583670af5c0f59f5
anakin_karrot <anakin@daangn.com>
2025-08-19 22:32:27 +0900

    use tokens

--- demo/src/components/LoadingSpinner.css.ts
+++ demo/src/components/LoadingSpinner.css.ts
@@ -1,4 +1,5 @@
-import { style, keyframes } from "@vanilla-extract/css";
+import { vars } from "@seed-design/design-token";
+import { keyframes, style } from "@vanilla-extract/css";
 
 const spin = keyframes({
   "0%": { transform: "rotate(0deg)" },
@@ -8,8 +9,8 @@ const spin = keyframes({
 export const spinner = style({
   width: 48,
   height: 48,
-  border: "4px solid #ccc",
-  borderTop: "4px solid #333",
+  border: `4px solid ${vars.$scale.color.gray600}`,
+  borderTop: `4px solid ${vars.$scale.color.gray900}`,
   borderRadius: "50%",
   animation: `${spin} 1s linear infinite`,
 });
