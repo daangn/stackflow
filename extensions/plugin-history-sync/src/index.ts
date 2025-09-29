83bd8e4c8bf499cb2674fc84fcacd0f8e9f5b1da
anakin_karrot <anakin@daangn.com>
2025-09-29 16:07:21 +0900

    hide navigation process

--- extensions/plugin-history-sync/src/index.ts
+++ extensions/plugin-history-sync/src/index.ts
@@ -2,11 +2,6 @@ export { useHistoryTick } from "./HistoryQueueContext";
 export * from "./historySyncPlugin";
 export { useInitialSetupProcessStatus } from "./InitialSetupProcessStatusContext";
 export { makeTemplate, UrlPatternOptions } from "./makeTemplate";
-export {
-  isTerminated,
-  type NavigationProcess,
-  NavigationProcessStatus,
-} from "./NavigationProcess/NavigationProcess";
 export { Route, RouteLike } from "./RouteLike";
 export { useRoutes } from "./RoutesContext";
 export { useIsRenderInTransition } from "./useIsRenderInTransition";
