bbcb78b58d1674df1db2b32cf8e8c327a2ad0067
anakin_karrot <anakin@daangn.com>
2025-12-10 01:10:37 +0900

    fix type

--- integrations/react/src/__internal__/ActivityComponentType.ts
+++ integrations/react/src/__internal__/ActivityComponentType.ts
@@ -1,6 +1,15 @@
 import type { MonolithicActivityComponentType } from "./MonolithicActivityComponentType";
-import type { StructuredActivityComponentType } from "./StructuredActivityComponentType";
+import {
+  isStructuredActivityComponent,
+  type StructuredActivityComponentType,
+} from "./StructuredActivityComponentType";
 
 export type ActivityComponentType<T extends {} = {}> =
   | MonolithicActivityComponentType<T>
   | StructuredActivityComponentType<T>;
+
+export function isMonolithicActivityComponentType<T extends {}>(
+  componentType: ActivityComponentType<T>,
+): componentType is MonolithicActivityComponentType<T> {
+  return !isStructuredActivityComponent(componentType);
+}
--- integrations/react/src/__internal__/MonolithicActivityComponentType.ts
+++ integrations/react/src/__internal__/MonolithicActivityComponentType.ts
@@ -4,3 +4,9 @@ import type { StaticActivityComponentType } from "./StaticActivityComponentType"
 export type MonolithicActivityComponentType<T extends {} = {}> =
   | StaticActivityComponentType<T>
   | LazyActivityComponentType<T>;
+
+export function isLazyActivityComponentType<T extends {}>(
+  componentType: MonolithicActivityComponentType<T>,
+): componentType is LazyActivityComponentType<T> {
+  return "_load" in componentType;
+}
