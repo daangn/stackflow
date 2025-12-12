cf49756a6ed2fa3ece342759efbb1b6864eaf873
anakin_karrot <anakin@daangn.com>
2025-12-12 16:28:12 +0900

    refactor

--- integrations/react/src/future/lazy.tsx
+++ integrations/react/src/future/lazy.tsx
@@ -1,35 +1,23 @@
 import type { LazyActivityComponentType } from "../__internal__/LazyActivityComponentType";
 import type { StaticActivityComponentType } from "../__internal__/StaticActivityComponentType";
-import { optimizableLazyComponent } from "../__internal__/utils/OptimizableLazyComponent";
-import {
-  inspect,
-  makeSyncInspectable,
-  PromiseStatus,
-  type SyncInspectablePromise,
-} from "../__internal__/utils/SyncInspectablePromise";
+import { preloadableLazyComponent } from "../__internal__/utils/PreloadableLazyComponent";
+import { liftValue } from "../__internal__/utils/SyncInspectablePromise";
 
 export function lazy<T extends { [K in keyof T]: any } = {}>(
   load: () => Promise<{ default: StaticActivityComponentType<T> }>,
 ): LazyActivityComponentType<T> {
-  let cachedValue: SyncInspectablePromise<{
-    default: StaticActivityComponentType<T>;
-  }> | null = null;
-
-  const cachedLoad = () => {
-    if (
-      !cachedValue ||
-      inspect(cachedValue).status === PromiseStatus.REJECTED
-    ) {
-      cachedValue = makeSyncInspectable(load());
-    }
-
-    return cachedValue;
-  };
+  const { Component, preload } = preloadableLazyComponent(() =>
+    liftValue(load()),
+  );
 
   const LazyActivityComponent: LazyActivityComponentType<T> = Object.assign(
-    optimizableLazyComponent(cachedLoad),
+    Component,
     {
-      _load: cachedLoad,
+      _load: async () => {
+        await preload();
+
+        return { default: Component };
+      },
     },
   );
 
