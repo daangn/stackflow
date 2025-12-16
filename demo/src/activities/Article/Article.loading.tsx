82ce76ba55a36682556c98b84f6c738e67d73e7f
anakin_karrot <anakin@daangn.com>
2025-12-16 15:58:31 +0900

    fix demo

--- demo/src/activities/Article/Article.loading.tsx
+++ demo/src/activities/Article/Article.loading.tsx
@@ -4,7 +4,6 @@ import LoadingSpinner from "../../components/LoadingSpinner";
 import * as css from "./Article.loading.css";
 
 const ArticleLoading = loading<"Article">(() => {
-  console.log("ArticleLoading");
   return (
     <div className={css.container}>
       <LoadingSpinner />
