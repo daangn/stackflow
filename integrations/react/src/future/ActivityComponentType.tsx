55c779822d362f6ec66640f3d0be07061d6c9087
JH.Lee <contact@jins.dev>
2026-01-16 16:37:53 +0900

    rollback

--- integrations/react/src/future/ActivityComponentType.tsx
+++ integrations/react/src/future/ActivityComponentType.tsx
@@ -2,9 +2,7 @@ import type {
   InferActivityParams,
   RegisteredActivityName,
 } from "@stackflow/config";
-import type { MonolithicActivityComponentType } from "../__internal__/MonolithicActivityComponentType";
-import type { StructuredActivityComponentType } from "../__internal__/StructuredActivityComponentType";
+import type { ActivityComponentType as ActivityComponentTypeInternal } from "../__internal__/ActivityComponentType";
 
 export type ActivityComponentType<ActivityName extends RegisteredActivityName> =
-  | MonolithicActivityComponentType<InferActivityParams<ActivityName>>
-  | StructuredActivityComponentType<InferActivityParams<ActivityName>>;
+  ActivityComponentTypeInternal<InferActivityParams<ActivityName>>;
