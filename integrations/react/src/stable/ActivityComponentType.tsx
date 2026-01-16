46779ee2a8b68b0aa69b7691eb8e082b351d283e
JH.Lee <contact@jins.dev>
2026-01-16 17:04:29 +0900

    enhance

--- integrations/react/src/stable/ActivityComponentType.tsx
+++ integrations/react/src/stable/ActivityComponentType.tsx
@@ -1,4 +1,4 @@
 import type { StaticActivityComponentType } from "../__internal__/StaticActivityComponentType";
 
-export type ActivityComponentType<T extends { [K in keyof T]: any } = {}> =
+export type ActivityComponentType<T extends {} = {}> =
   StaticActivityComponentType<T>;
