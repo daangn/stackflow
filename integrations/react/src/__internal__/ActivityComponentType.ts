3bdc21f0b59152f592c57f96b7eb1bd6de1ba670
JH.Lee <contact@jins.dev>
2026-01-19 12:27:01 +0900

    rollback

--- integrations/react/src/__internal__/ActivityComponentType.ts
+++ integrations/react/src/__internal__/ActivityComponentType.ts
@@ -1,6 +1,6 @@
 import type { MonolithicActivityComponentType } from "./MonolithicActivityComponentType";
 import type { StructuredActivityComponentType } from "./StructuredActivityComponentType";
 
-export type ActivityComponentType<T extends {} = {}> =
+export type ActivityComponentType<T extends { [K in keyof T]: any } = {}> =
   | MonolithicActivityComponentType<T>
   | StructuredActivityComponentType<T>;
